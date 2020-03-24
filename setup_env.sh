287bd7f7112207509cf7af6be867296ab921688a
David J. Gardner <gardner48@llnl.gov>
2020-03-23 18:49:44 -0700

    update env script for laptop

--- setup_env.sh
+++ setup_env.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-loadspack sunenv
+if [ "$HOST" == "Sandcastle.llnl.gov" ]; then
 
-spack load gcc@9.2.0
-spack load openmpi%gcc@9.2.0
+    source ~/.bashrc
 
-spack load hypre%gcc@9.2.0~int64
-export HYPRE_DIR="$(spack location -i hypre%gcc@9.2.0~int64)"
-export HYPRE_LIBRARY_FILE=libHYPRE.so
+    module load gcc-9.2.0
+    export CC=gcc
 
-export CC=gcc
-export MPICC=mpicc
-export MPICXX=mpicxx
+    module load openmpi-3.1.5
+    export MPICC=mpicc
+    export MPICXX=mpicxx
+
+    module load hypre-2.18.2-int32
+    export HYPRE_DIR=${HYPRE_ROOT}
+    export HYPRE_LIBRARY_FILE=libHYPRE.a
+
+else
+
+    loadspack sunenv
+
+    spack load gcc@9.2.0
+    export CC=gcc
+
+    spack load openmpi%gcc@9.2.0
+    export MPICC=mpicc
+    export MPICXX=mpicxx
+
+    spack load hypre%gcc@9.2.0~int64
+    export HYPRE_DIR="$(spack location -i hypre%gcc@9.2.0~int64)"
+    export HYPRE_LIBRARY_FILE=libHYPRE.so
+
+fi
